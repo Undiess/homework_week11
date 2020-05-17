e5d1ae7ecba3364da2c154032ad5fbfc68410b2b
Undiess <nguyen.andy67@gmail.com>
2020-05-18 00:30:29 +0930

    managed to store inputs in file

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var app = express()
 var path = require("path");
 const fs = require('fs'); 
 
-const public_dir = __dirname + `/public/`;
-const output_dir = __dirname + `/db/`;
+
+const dir= __dirname + `/public/`;
+const dbdir = __dirname+`/db/`;
+
 
 var PORT = process.env.PORT ||8080
 
@@ -15,25 +17,29 @@ app.use(express.static('public'));
 
 
 app.get("/notes",function (req,res){
-    res.sendFile("/public/notes.html"));
+    res.sendFile(path.join(dir, "notes.html"));
 })
 app.get("*",function (req,res){
-    res.sendFile("/public/index.html"));
+    res.sendFile(path.join(dir, "index.html"));
     
 })
 
 app.get("/api/notes",function (req,res){
     fs.readFile('./db/db.json',function(err,data){
-       
-        return res.json(JSON.parse(data))
-    
+        res.send(data)
     })
 })
 
-app.post("/api/notes",function(req, res){
-    console.log(req.body)
+app.post("/api/notes",function(req, res){ 
+    var newNote = JSON.stringify(req.body)
+    fs.appendFileSync(dbdir + '/db.json',newNote,function(err){
+        if (err) throw err;
+        console.log("added to DB")
+    })
+    console.log(newNote)
 
-})
+    
+})  
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
