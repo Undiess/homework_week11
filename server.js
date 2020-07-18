da3b6e7f061df6ed366ba047d0fa9a9da85a0316
Undiess <nguyen.andy67@gmail.com>
2020-07-18 16:20:24 +0930

    post notes working

--- server.js
+++ server.js
@@ -19,24 +19,46 @@ app.use(express.static('public'));
 app.get("/notes",function (req,res){
     res.sendFile(path.join(dir, "notes.html"));
 })
+app.get("/api/notes",function (req,res){
+    fs.readFile(dbdir + '/db.json','utf8',function(err,data){
+       res.send(JSON.parse(data))
+       console.log(JSON.parse(data))
+       console.log(data)
+       return JSON.parse(data)
+       
+    })
+    
+       
+      
+})
 app.get("*",function (req,res){
     res.sendFile(path.join(dir, "index.html"));
     
 })
 
-app.get("/api/notes",function (req,res){
-    fs.readFile('./db/db.json',function(err,data){
-        res.send(data)
-    })
-})
+
 
 app.post("/api/notes",function(req, res){ 
-    var newNote = JSON.stringify(req.body)
-    fs.appendFileSync(dbdir + '/db.json',newNote,function(err){
-        if (err) throw err;
-        console.log("added to DB")
+    console.log(req.body)
+    var id = 0;
+    fs.readFile(dbdir + '/db.json','utf8',function(err,data){
+        var notes = JSON.parse(data)
+        console.log(notes.length)
+        id = notes.length +1
+        req.body.id = id
+      
+        notes.push(req.body)
+        save= JSON.stringify(notes)
+        fs.writeFile((dbdir + '/db.json'),save,function(err){
+            if(err){
+                console.log(err)
+            }
+        })
     })
-    console.log(newNote)
+    
+    
+    
+    
 
     
 })  
@@ -45,4 +67,6 @@ app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });
+  
+
   
